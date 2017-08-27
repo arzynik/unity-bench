427ead102fa03956d51ae9758d34225566d7448b
Devin Smith <github@arzynik.com>
2017-08-27 14:07:47 -0700

    initial

---
+++ Assets/Cube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	private int alive = 0;
+	public Game game;
+
+	// Use this for initialization
+	void Start () {
+		//Explode(transform.position);
+		Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+		GetComponent<Renderer>().material.color = newColor;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		alive++;
+		transform.localScale += new Vector3(0.01f, 0.01f, 0.01f);
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Cube" || alive < 50) {
+			return;
+		}
+		game.Explode(gameObject, 
+		col.contacts[0].point);
+	}
+}
---
+++ Assets/Game.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+	public GameObject part;
+	private int spread = 5;
+	private int max = 500;
+	
+	void Update() {
+		float t = 1/Time.deltaTime;
+		if (t < 10) {
+			max--;
+		} else {
+			max++;
+		}
+		Debug.Log(max);
+	}
+	
+	public void Explode(GameObject go, Vector3 loc) {
+		int cubes = GameObject.FindGameObjectsWithTag("Cube").Length;
+		
+		if (cubes > max) {
+			DestroyObject(go);
+			return;
+		}
+		
+		for (int i = 0; i <= spread; i++) {
+			float rand = Random.Range(4, 90);
+			GameObject instance = Instantiate(part, loc, transform.rotation) as GameObject;
+			instance.GetComponent<Cube>().game = this;
+
+			Rigidbody[] ArrayRigs = instance.GetComponentsInChildren<Rigidbody>();
+			foreach (var item in ArrayRigs) {
+				Rigidbody rb = item.GetComponent<Rigidbody>();
+				rb.AddExplosionForce(2000.0f, new Vector3(loc.x + rand, loc.y - rand), 50f, 1f);
+			}
+
+			DestroyObject(go);
+			DestroyObject(instance, Random.Range(1f, 15f));
+		}
+	}
+}
